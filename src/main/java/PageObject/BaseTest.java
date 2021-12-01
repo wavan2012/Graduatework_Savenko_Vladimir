253d3a7f38a4295c3eb08f759d69926e6f7f0f80
wavan2012 <wavan2012@gmail.com>
2021-12-01 14:55:24 +0300

    edit pom.xml

--- src/main/java/PageObject/BaseTest.java
+++ src/main/java/PageObject/BaseTest.java
@@ -1,12 +1,10 @@
 package PageObject;
 
 import driver.DriverCreation;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
-    static WebDriver driver;
     @BeforeTest
     public void beforeTest(){
        MainPage.openPage();
